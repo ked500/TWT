fd942803663eee962c22e4cfe665a4c761b5ec59
ked500 <ked500@tut.by>
2022-03-20 00:48:29 +0300

    Added mood bar

---
+++ TWT/Presentation Layer/Coloring.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+
+namespace TWT
+{
+    public static class Coloring
+    {
+        public static Color SetColors(float value)
+        {
+            int red = 255, green = 255, blue = 255;
+
+            if (float.IsNaN(value))
+                return Color.Gray;
+            if(value > 0)
+            {   //0 to 0.5
+                if(value < 0.5)
+                {
+                    red = 255;
+                    green = 255;
+                    blue = Convert.ToInt32(255 - 255 * value * 2);
+                }
+                //0.5 to 1.5
+                else if (value >= 0.5)
+                {
+                    red = 255;
+                    blue = 0;
+                    green = Convert.ToInt32(255 - 255 * (value - 0.5f));
+                }
+            }
+            else if(value < 0)
+            {
+                //0 to -0.5
+                if (value > -0.5)
+                {
+                    blue = 255;
+                    green = 255;
+                    red = Convert.ToInt32(255 - 255 * Math.Abs(value * 2));
+                }
+                //-0.5 to -1.5
+                else if (value <= -0.5)
+                {
+                    blue = 255;
+                    red = 0;
+                    green = Convert.ToInt32(255 - 255 * Math.Abs((value + 0.5f)));
+                }
+            }
+            return Color.FromArgb(IsOutofByte(red), IsOutofByte(green), IsOutofByte(blue));
+        }
+
+        private static int IsOutofByte(int value)
+        {
+            if (value < 0)
+                return 0;
+            if (value > 255)
+                return 255;
+            return value;
+        }
+    }
+}
--- TWT/Presentation Layer/MainForm.cs
+++ TWT/Presentation Layer/MainForm.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.ToolTips;
+using GMap.NET.WindowsForms.Markers;
+
 
 namespace TWT
 {
@@ -37,7 +43,6 @@ namespace TWT
             }
         }
 
-
         //File List
         private void ChooseFileLIstClick(object sender, MouseEventArgs e)
         {
@@ -47,6 +52,58 @@ namespace TWT
         private void ChooseFileListChangeIndex(object sender, EventArgs e)
         {
 
+        }
+
+        //GMap
+        private void FormLoad(object sender, EventArgs e)
+        {
+            gMapControl.ShowCenter = false;
+        }
+
+        //Emotional Panel
+
+        private void emotionalPanelPaint(object sender, PaintEventArgs e)
+        {
+          DrawPanel();
+        }
+
+        private void DrawPanel()
+        {
+            //Pen
+            Pen pen = new Pen(Color.Black, 0.002f);
+            //Amount of grids
+            int grids = 6;
+            int widthofRec = (emotionalPanel.Width - 1) / grids;
+
+            Graphics g = emotionalPanel.CreateGraphics();
+            Rectangle[] rect = new Rectangle[grids];
+            Brush[] brush = new LinearGradientBrush[grids];
+            float min = -1.5f, step = 0.5f;
+            float currentValue = min;
+            //String Above panel
+            g.DrawString("Емоциональный вес", new Font("HelvLight", 10), Brushes.White, (emotionalPanel.Width + 36) / 4, 0);
+            for(int i = 0; i < grids; i++)
+            {
+                rect[i] = new Rectangle(widthofRec * i, 30, widthofRec, 20);
+                brush[i] = new LinearGradientBrush(rect[i], 
+                    Coloring.SetColors(currentValue + 0.0001f),
+                    Coloring.SetColors(currentValue + step - 0.0001f), 0f);
+                currentValue += step;
+                g.FillRectangle(brush[i], rect[i]);
+                g.DrawRectangle(pen, rect[i]);
+                g.DrawString(Convert.ToString(currentValue - step), new Font("Arial", 8), Brushes.White, (widthofRec - 2) * i, 18);
+            }
+            //String for last grid
+            g.DrawString(Convert.ToString(currentValue) + "+", new Font("Arial", 8), Brushes.White, (widthofRec - 3) * grids, 18);
+        }
+
+        private void emotionalPanelShow_CheckedChanged(object sender, EventArgs e)
+        {
+            //Hiding Emotional Panel
+            if(emotionalPanel.Visible)
+              emotionalPanel.Visible = false;
+            else
+              emotionalPanel.Visible = true;
         }
     }
 }
