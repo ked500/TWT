7cb10986562e1a5f340562aa92a494152a02a93d
ked500 <ked500@tut.by>
2022-03-22 02:02:38 +0300

    Added DB class

---
+++ TWT/Data Layer/DB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using TWT.Business_Layer.Models;
+
+namespace TWT.Data_Layer
+{
+    internal class DB
+    {
+        static public List<GMapPolygon> GetStates(List<State> states)
+        {
+            List<GMapPolygon> polygons = new List<GMapPolygon>();
+            foreach (var state in states)
+            {
+                foreach (var _polygons in state.Polygons)
+                {
+                    foreach (Polygon polygon in _polygons)
+                    {
+                        List<PointLatLng> vertexes = new List<PointLatLng>();
+                        foreach (var vertex in polygon.Vertexes)
+                        {
+                            PointLatLng vert = new PointLatLng(vertex.Item1, vertex.Item2);
+                            vertexes.Add(vert);
+                        }
+                        GMapPolygon pol = new GMapPolygon(vertexes, "");
+                    }
+                }
+            }
+            return polygons;
+        }
+    }
+}
