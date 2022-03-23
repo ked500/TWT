757933412746cb88c398da72a367c30a49df0c2e
Сергей Тимошко <sergeytimoshko2512@gmail.com>
2022-03-23 22:09:08 +0300

    added DB

--- TWT/Data Layer/DB.cs
+++ TWT/Data Layer/DB.cs
@@ -7,11 +7,56 @@ using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using TWT.Business_Layer.Models;
+using TWT.Data_Layer.Parsers;
+
 
 namespace TWT.Data_Layer
 {
     internal class DB
     {
+        private List<State> states = new List<State>();
+        private Dictionary<string, List<Tweet>> tweets = new Dictionary<string, List<Tweet>>();
+        private Dictionary<string, double> sentiments = new Dictionary<string, double>();
+        public List<State> States
+        {
+            get
+            {
+                return states;
+            }
+            set
+            {
+                states = value;
+            }
+        }
+        public Dictionary<string, List <Tweet>> Tweets
+        {
+            get
+            {
+                return tweets;
+            }
+            set
+            {
+                tweets= value;
+            }
+        }
+        public Dictionary<string, double> Sentiments
+        {
+            get
+            {
+                return sentiments;
+            }
+            set
+            {
+                sentiments = value;
+            }
+        }
+        DB()
+        {
+            Sentiments = SentimentsParser.Parse("");
+            States = JsonParser.ParseStates();
+        }
+
+
         static public List<GMapPolygon> GetStates(List<State> states)
         {
             List<GMapPolygon> polygons = new List<GMapPolygon>();
@@ -31,5 +76,6 @@ namespace TWT.Data_Layer
             }
             return polygons;
         }
+
     }
 }
