1213847ef2c8b0b3aba496167d4f53351d02aad4
Ash1VT <nikita.panasevitch@yandex.by>
2022-03-30 21:49:59 +0300

    Added DAO objects

---
+++ TWT/Data Layer/DAO/SentimentsDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TWT.Data_Layer.DAO
+{
+    public class SentimentsDAO
+    {
+        public Dictionary<string, double> GetSentiments()
+        {
+            return DB.GetInstance().Sentiments;
+        }
+
+
+    }
+}
---
+++ TWT/Data Layer/DAO/StateDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWT.Business_Layer.Models;
+
+namespace TWT.Data_Layer.DAO
+{
+    public class StateDAO
+    {
+        public List<State> GetStates()
+        {
+            return DB.GetInstance().States;
+        }
+
+
+
+        //CLEAR ALL TWEETS IN ALL STATES
+        public void ClearTweetsInStates()
+        {
+            GetStates().ForEach((x) => x.Tweets.Clear());
+        }
+
+        public bool InsertTweet(State State, Tweet Tweet)
+        {
+            try
+            {
+                State findedState = GetStates().Find((x) => x == State);
+                findedState.Tweets.Add(Tweet);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
