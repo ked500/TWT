ddcca9c1289ae1d6d743b5266f7543395f77385e
Ash1VT <nikita.panasevitch@yandex.by>
2022-03-30 21:49:18 +0300

    Separated business logic

---
+++ TWT/Business Layer/Business Analyzers/Analyzer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWT.Business_Layer.Models;
+using TWT.Data_Layer.DAO;
+
+namespace TWT.Business_Layer.Business_Analyzers
+{
+    public class Analyzer : BusinessObject
+    {
+        private void AnalyzeTweets()
+        {
+            foreach (var tweet in tweetDAO.GetTweets())
+            {
+                tweet.Analyse(sentimentsDAO.GetSentiments());
+            }
+        }
+
+
+                
+        private bool SetState(Tweet tweet)
+        {
+            foreach (var state in stateDAO.GetStates())
+            {
+                if (state.isInside(tweet))
+                {
+                    stateDAO.InsertTweet(state, tweet);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Update(string tweetFileName)
+        {
+            stateDAO.ClearTweetsInStates();
+            tweetDAO.ClearUnknownTweets();
+
+            try
+            {
+                tweetDAO.UpdateTweets(tweetFileName);
+            }
+            catch
+            {
+                return false;
+            }
+
+            this.AnalyzeTweets();
+
+            foreach (var tweet in tweetDAO.GetTweets())
+            {
+                if (!SetState(tweet))
+                    tweetDAO.InsertUnknownTweet(tweet);
+            }
+
+            return true;
+        }
+
+
+    }
+}
